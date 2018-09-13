fb443133c258f405ab359b2880e146f2858f0592
Tobias Burger <tobias_burger@hotmail.com>
2018-09-13 21:56:34 +0200

    port from javascript code and use parcel

---
+++ app.tsx
@@ -0,0 +1,223 @@
+import * as React from "react";
+import ReactDOM from "react-dom";
+import { withReducer, onlyUpdateForKeys } from "recompose";
+import * as GameLogic from "./gameLogic";
+import "./app.css";
+import {
+  Board,
+  Player,
+  Cell,
+  GameOver,
+  GameState,
+  empty,
+  player
+} from "./types";
+
+// import PlayerXImage from "./assets/PlayerX.svg";
+// import PlayerOImage from "./assets/PlayerO.svg";
+// import RestartImage from "./assets/restart.png";
+const PlayerXImage = require("./assets/PlayerX.svg");
+const PlayerOImage = require("./assets/PlayerO.svg");
+const RestartImage = require("./assets/restart.png");
+
+interface State {
+  board: Board;
+  currentPlayer: Player;
+  winner: GameState;
+}
+
+type OnMove = (x: number, y: number) => void;
+
+type Action = { type: "MOVE"; x: number; y: number } | { type: "RESTART" };
+
+const initialState: State = {
+  board: [
+    [empty(), empty(), empty()],
+    [empty(), empty(), empty()],
+    [empty(), empty(), empty()]
+  ],
+  currentPlayer: "X",
+  winner: { type: "CONTINUE" }
+};
+
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case "MOVE":
+      if (!GameLogic.canUpdateBoard(state.board, action.x, action.y))
+        return state;
+      const newBoard = GameLogic.updateBoard(
+        state.board,
+        action.x,
+        action.y,
+        player(state.currentPlayer)
+      );
+      const winner = GameLogic.getWinner(newBoard);
+      return {
+        ...state,
+        board: newBoard,
+        currentPlayer: state.currentPlayer === "X" ? "O" : "X",
+        winner
+      };
+    case "RESTART":
+      return initialState;
+    default:
+      return state;
+  }
+};
+
+const prefetchImages = () =>
+  [PlayerXImage, PlayerOImage, RestartImage].map(img => (
+    <link key={img} rel="prefetch" href={img} />
+  ));
+
+const PlayerX = ({ className, ...props }: any) => (
+  <img
+    {...props}
+    alt="PlayerX"
+    src={PlayerXImage}
+    className={["PlayerX", className].join(" ")}
+  />
+);
+const PlayerO = ({ className, ...props }: any) => (
+  <img
+    {...props}
+    alt="PlayerO"
+    src={PlayerOImage}
+    className={["PlayerO", className].join(" ")}
+  />
+);
+const NoPlayer = (props: any) => <span {...props} className="NoPlayer" />;
+
+interface CellProps {
+  value: Cell;
+  x: number;
+  y: number;
+  onMove: OnMove;
+}
+
+const Cell = onlyUpdateForKeys<CellProps>(["value"])(
+  ({ value, x, y, onMove }: CellProps) => {
+    const Child = () => {
+      switch (value.type) {
+        case "PLAYER":
+          switch (value.player) {
+            case "X":
+              return <PlayerX />;
+            case "O":
+              return <PlayerO />;
+          }
+        case "EMPTY":
+          return <NoPlayer />;
+      }
+    };
+    return (
+      <button
+        className="Cell"
+        disabled={value.type !== "EMPTY"}
+        onClick={e => onMove(x, y)}
+      >
+        <Child />
+      </button>
+    );
+  }
+);
+
+const DispatchPlayer = ({ player }: { player: Player }) =>
+  player === "X" ? (
+    <PlayerX className="PlayerX--Small" />
+  ) : (
+    <PlayerO className="PlayerO--Small" />
+  );
+
+const CurrentPlayer = ({ currentPlayer }: { currentPlayer: Player }) => (
+  <div className="CurrentPlayer">
+    <span className="CurrentPlayer__Text">
+      Player:
+      <DispatchPlayer player={currentPlayer} />
+    </span>
+  </div>
+);
+
+const Board = ({ board, onMove }: { board: Board; onMove: OnMove }) => (
+  <div>
+    {board.map((ys, x) => (
+      <div key={`${x}`} className="Board__Row">
+        {ys.map((v, y) => (
+          <Cell key={`${x}-${y}`} {...{ board, value: v, x, y, onMove }} />
+        ))}
+      </div>
+    ))}
+  </div>
+);
+
+const Game = ({
+  board,
+  onMove,
+  currentPlayer
+}: {
+  board: Board;
+  onMove: OnMove;
+  currentPlayer: Player;
+}) => (
+  <div>
+    <Board board={board} onMove={onMove} />
+    <CurrentPlayer currentPlayer={currentPlayer} />
+  </div>
+);
+
+const GameOver = ({
+  winner,
+  onRestart
+}: {
+  winner: GameOver;
+  onRestart: () => void;
+}) => (
+  <div className="GameOver">
+    <img
+      className="GameOver__Image"
+      onClick={e => onRestart()}
+      src={RestartImage}
+      alt="Restart"
+    />
+    <p className="GameOver__Text">
+      {winner.type === "DRAW" ? (
+        "It's a draw!"
+      ) : (
+        <span>
+          Player
+          <DispatchPlayer player={winner.player} />
+          wins!
+        </span>
+      )}
+    </p>
+  </div>
+);
+
+const enhance = withReducer<undefined, State, Action, "state", "dispatch">(
+  "state",
+  "dispatch",
+  reducer,
+  initialState
+);
+
+const App = enhance(({ state, dispatch }) => (
+  <div className="App">
+    {prefetchImages()}
+    {state.winner.type === "GAME_OVER" ? (
+      <GameOver
+        winner={state.winner.gameOver}
+        onRestart={() => dispatch({ type: "RESTART" })}
+      />
+    ) : (
+      <Game
+        board={state.board}
+        currentPlayer={state.currentPlayer}
+        onMove={(x, y) => dispatch({ type: "MOVE", x, y })}
+      />
+    )}
+  </div>
+));
+
+// export default App;
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ gameLogic.ts
@@ -0,0 +1,108 @@
+// This lib contains powerful functions for list transformations.
+import * as R from "ramda";
+import { Board, Cell, gameOver, winner, player, GameState } from "./types";
+
+/**
+ * Helps debugging the board
+ */
+export const logBoard = (board: Board): Board => {
+  console.table(board);
+  return board;
+};
+
+/**
+ * Updates a single board value by x and y coordinates and returns the modified board.
+ * Leaves the original board unchanged.
+ */
+export const updateBoard = (
+  board: Board,
+  x: number,
+  y: number,
+  value: Cell
+): Board => R.assocPath<Cell, Board>([x, y], value)(board);
+
+/**
+ * Gets a single board value by x and y coordinates
+ */
+export const getBoard = (board: Board, x: number, y: number): Cell | null =>
+  R.path<Cell>([x, y])(board) || null;
+
+/**
+ * Checks if a board value can be updated (simply checks if the board contains null)
+ */
+export const canUpdateBoard = (board: Board, x: number, y: number): boolean =>
+  R.equals(getBoard(board, x, y), { type: "EMPTY" });
+
+/**
+ * Checks if all elements a row contain the same value 'v'.
+ * Checks all three rows.
+ */
+const checkRowsForWinner = (board: Board, v: Cell): boolean => {
+  const check = R.all(R.equals(v));
+  return check(board[0]) || check(board[1]) || check(board[2]);
+};
+
+/**
+ * Cheks if all elements of a column contain the same value 'v'.
+ * Checks all three columns.
+ *
+ * Uses a simple trick by transposing the board so that the whole board
+ * gets rotated and the columns become rows. Then the "rows" can be passed
+ * to checkRowsForWinner
+ */
+const checkColsForWinner = (board: Board, v: Cell): boolean => {
+  const transposedBoard = R.transpose(board);
+  return checkRowsForWinner(transposedBoard, v);
+};
+
+/**
+ * Checks if all elements of a diagnola contain the same value 'v'.
+ * Checks the two diagonals.
+ */
+const checkDiagonalsForWinner = (board: Board, v: Cell): boolean => {
+  const diag1 = [board[0][0], board[1][1], board[2][2]];
+  const diag2 = [board[2][0], board[1][1], board[0][2]];
+  const check = R.all(R.equals(v));
+  return check(diag1) || check(diag2);
+};
+
+/**
+ * Checks if any one of the
+ * * rows
+ * * columns
+ * * diagonals
+ * of the board contain the same value 'v'.
+ */
+const checkForWinner = (board: Board, v: Cell): boolean =>
+  checkRowsForWinner(board, v) ||
+  checkColsForWinner(board, v) ||
+  checkDiagonalsForWinner(board, v);
+
+/**
+ * Checks if the game is a draw
+ *
+ * It does check if all of the boards aren't null.
+ */
+const checkForDraw = (board: Board): boolean => {
+  const check = R.all<Cell>(x => !R.equals(x, { type: "EMPTY" }));
+  return check(R.flatten<Cell>(board));
+};
+
+/**
+ * Determines either the winner or a draw.
+ * If null is returned the game continues.
+ *
+ * @returns "X" | "O" | "DRAW" | null
+ */
+export const getWinner = (board: Board): GameState => {
+  if (checkForWinner(board, player("X")))
+    // player X wins!
+    return gameOver(winner("X"));
+  else if (checkForWinner(board, player("O")))
+    // player O wins!
+    return gameOver(winner("O"));
+  else if (checkForDraw(board))
+    // it'a a draw!
+    return { type: "GAME_OVER", gameOver: { type: "DRAW" } };
+  else return { type: "CONTINUE" };
+};
